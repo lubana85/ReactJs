ca9dd782a0c3de128498cc163e5c636704163a28
lubana85 <32568076+lubana85@users.noreply.github.com>
2018-08-01 10:53:19 +1000

    testing Photo component

--- test/Photo.test.js
+++ test/Photo.test.js
@@ -3,8 +3,14 @@ import renderer from 'react-test-renderer';
 import Photo from './../components/Photo';
 
 test('Photo displays the title', () => {
+  
+  const item = {
+        title: 'photo title',
+        url: 'http://placehold.it/600/92c952',
+        thumbnailUrl: 'http://placehold.it/150/92c952',
+    };
   const component = renderer.create(
-    <Photo title="test" />,
+    <Photo {...item} />,
   );
   let tree = component.toJSON();
   expect(tree).toMatchSnapshot();
