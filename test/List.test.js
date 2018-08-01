09c8b91c2b687ae341274f16f622b6d90f34911a
lubana85 <32568076+lubana85@users.noreply.github.com>
2018-08-01 11:10:18 +1000

    testing List component

--- test/List.test.js
+++ test/List.test.js
@@ -2,25 +2,43 @@ import React from 'react';
 import {shallow} from "enzyme";
 import toJson from "enzyme-to-json";
 import List from './../components/List';
+import { mapStateToProps, mapDispatchToProps } from './../components/List.js';
+import { LIST_START } from './../actions/photo.js';
 
 describe('List component renders a list of photos', () => {
   it('renders correctly', () => {
     const wrapper = shallow(<List/>);
-    const loading = true;
+			    
+    // Test Initial State:
+     const initialState = {
+          loading: true,
+          data: [],
+      };
+
+      expect(mapStateToProps(initialState).loading).toEqual(true);
+
+      const dispatch = jest.fn();
+
+      mapDispatchToProps(dispatch).getData();
+      expect(dispatch.mock.calls[0][0]).toEqual({ type: 'LIST_START'});
+	  
+    // Test state with data
+    const loading = false;
 
     const data = [
-      {title: "x"},
-      {title: "y"},
-      {title: "z"}
+      {title: "x", url: "url", thumbnailUrl: "thumbnailUrl"},
+      {title: "y", url: "url", thumbnailUrl: "thumbnailUrl"},
+      {title: "z", url: "url", thumbnailUrl: "thumbnailUrl"}
     ];
     consst error = '';
 
     wrapper.setState({
       loading,
       data,
-	 error
+      error
     });
 
     expect(toJson(wrapper)).toMatchSnapshot();
+	  
   });
 });
